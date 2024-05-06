44dcaee1d12cd95eb45a2a23f1d0d450af75294a
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-05-06 01:18:52 -0400

    add types for api params and modify job types

--- src/types/job.dto.ts
+++ src/types/job.dto.ts
@@ -1,13 +1,30 @@
-export interface spendCommodityJob {
+export interface spendCommodity {
   utxos: UtxoQuery[];
 }
 
-export interface tokenizeCommodityJob {
+export interface spendCommodityJob {
+  id: string;
+  utxos: UtxoQuery[];
+}
+
+export interface tokenizeCommodity {
   tokenName: string;
   metadataReference: string;
 }
 
+export interface tokenizeCommodityJob {
+  id: string;
+  tokenName: string;
+  metadataReference: string;
+}
+
+export interface recreateCommodity {
+  utxos: UtxoQuery[];
+  newDataReferences: string[];
+}
+
 export interface recreateCommodityJob {
+  id: string;
   utxos: UtxoQuery[];
   newDataReferences: string[];
 }
